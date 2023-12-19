3a08e10b1d290b8c82b607dcfaf617e14c4ce4ef
ewanmalone <70751521+ewanmalone@users.noreply.github.com>
2023-12-19 11:46:09 +0000

    Create Install-Devapps.ps1

---
+++ Install-Devapps.ps1
@@ -0,0 +1,50 @@
+################################################################################
+##  File:  Install-AVDapps.ps1
+##  Desc:  Choco installs for AVD applications
+################################################################################
+Write-Output "Installing AVD Apps no additional parameters"
+$uninstall = false
+$choco_apps = @(
+    "git",
+    "gitextensions",
+    "oraclejdk",
+    "oracle-sql-developer",
+    "sql-server-management-studio",
+    "vscode",
+    "visualstudio2017community",
+    "winmerge",
+    "7zip", 
+    "adobereader", 
+    "azure-data-studio",
+    "duo-authentication",
+    "filezilla",
+    "googlechrome",
+    "microsoft-edge",
+    "fslogix",
+    "msvisualcplusplus2012-redist",
+    "msvisualcplusplus2013-redist",
+    "vcredist140",
+    "microsoft-build-tools",
+    "notepadplusplus"
+)
+
+foreach ($app in $choco_apps) {
+    Write-Host "Installing ${app} using Chocolatey binaries"
+    $ver = $app -split ":"
+    if ($uninstall) {
+        if ($ver.count -gt 1) {
+            choco uninstall $ver[0] --version=$ver[1]
+        }
+        else {
+            choco uninstall $app
+        }
+    }
+    else {
+        if ($ver.count -gt 1) {
+            choco install $ver[0] --version=$ver[1]
+        }
+        else {
+            choco install $app
+        }
+    }
+}
